71d93aaab75309af8afbba8f60e04e684343e39d
Jain <tanijain@deloitte.com>
2020-07-20 13:06:41 +0530

    Add external Style sheets

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ var path = require('path');
 
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 require('./route')(app);
-
-
 app.listen(process.env.PORT || 8080);
 
 console.log('Application is running on port 8080');
